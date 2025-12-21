c5d0ebc6605176fe004016316636793abb5f597f
Moussa Sangare <moussa.sangare.ma@gmail.com>
2025-12-21 01:54:29 +0000

    corrected mail module to include email transport service

--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -1,11 +1,14 @@
 import { Module, Global } from "@nestjs/common";
 import { MailService } from "./mail.service";
 import { ConfigModule } from "@nestjs/config";
+import { EmailTransportService } from "./email-transport.service";
 
 @Global()
 @Module({
   imports: [ConfigModule],
-  providers: [MailService],
-  exports: [MailService],
+  providers: [MailService,
+    EmailTransportService
+  ],
+  exports: [MailService, EmailTransportService],
 })
 export class MailModule {}
