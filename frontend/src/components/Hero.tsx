a83c4932208e48339c3f49ebcb215c955ddb1fb4
Moussa Sangare <moussa.sangare.ma@gmail.com>
2025-12-28 23:50:41 +0000

    corrected HeroImage from online to public images

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -117,7 +117,7 @@ const Hero = () => {
           className='absolute inset-0 bg-cover bg-center'
           style={{
             backgroundImage:
-              'url("https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&auto=format&fit=crop&w=2000&q=80")',
+              'url("/Heroimage.avif")',
           }}
         />
         <div className='absolute inset-0 bg-sky-900/70'></div>
